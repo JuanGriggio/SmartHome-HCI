0c557cb8a549a541e61dfb9ad4bc9763d41d3293
Juan Gabriel Griggio <juangriggio@gmail.com>
2020-05-10 12:14:19 -0300

    Improvements in myDevices page

--- components/quickAction.js
+++ components/quickAction.js
@@ -1,24 +1,22 @@
-VUE.component (
-    'quick-action',
-    {
-        props: {
-            name: {
-                type: String,
-                required: true
-            },
-            iconn: {
-                type: String,
-                required: true
-            }
-        },
-        template: {
-            <div>
-                <p>{{iconn}}</p>
-                <p id="npnm">{{name}}</p>
-            </div>  
-        },
-        data () {
-            return {}
-        }
-    }
-);
+// VUE.component ('quick-action', {
+//         props: {
+//             name: {
+//                 type: String,
+//                 required: true
+//             },
+//             iconn: {
+//                 type: String,
+//                 required: true
+//             }
+//         },
+//         template: 
+//             <div>
+//                 <p>{{iconn}}</p>
+//                 <p>{{name}}</p>
+//             </div>  
+//         ,
+//         data: function () {
+//             return {}
+//         }
+//     }
+// );
