f45830dcf667a33f01a38d7d13e3eb5ec18598fd
ignmendez-99 <nacho.mendez12345@gmail.com>
2020-05-15 19:35:40 -0300

    now the tutorial can go backwards

--- js/newUserWelcomeTutorial.js
+++ js/newUserWelcomeTutorial.js
@@ -3,13 +3,7 @@ var app = new Vue({
     vuetify: new Vuetify(),
     data: {
         items: ['JBL Speaker', 'Nava\'s Faucet', 'Soft blinds', 'Philips HUE lights', 'Pentagono door', 'GE Vacuum 5000', 'LG Fridge 7 million'],
-        firstStepCompleted: false,
-        secondStepCompleted: false,
-        thirdStepCompleted: false,
         currentStep: 1,
-        firstButtonDisabled: false,
-        secondButtonDisabled: true,
-        thirdButtonDisabled: true,
         dialog: false,
         rules: {
           required: value => !!value || 'Required.',
@@ -21,23 +15,6 @@ var app = new Vue({
         deviceSelected: ""
     },
     methods: {
-        enterSecondStep: function() {
-            this.firstStepCompleted = true;
-            this.currentStep = 2;
-            this.firstButtonDisabled = true;
-            this.secondButtonDisabled = false;
-        },
-        enterThirdStep: function() {
-            this.secondStepCompleted = true;
-            this.currentStep = 3;
-            this.secondButtonDisabled = true;
-            this.thirdButtonDisabled = false;
-        },
-        stepsCompleted: function() {
-            this.currentStep = 0;
-            this.thirdStepCompleted = true;
-            this.thirdButtonDisabled = true;
-        },
         registerRoom: function() {
           // .....
           // enterThirdStep();
